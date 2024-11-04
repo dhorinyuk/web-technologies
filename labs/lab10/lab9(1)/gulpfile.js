c2cb1bf1ed0efaffce97e91b5ef9a8db247caef1
dhorinyuk <dima.horinyuk27@gmail.com>
2024-11-04 09:25:02 +0200

    Додав 10 лабу

---
+++ labs/lab10/lab9(1)/gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+
+// Команда для компіляції Pug
+gulp.task('pug', function() {
+    return gulp.src('src/pug/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('dist'));
+});
+
+// Команда для компіляції SCSS
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
+});
+
+// Команда для копіювання зображень
+gulp.task('images', function() {
+    return gulp.src('src/png/**/*')
+        .pipe(gulp.dest('dist/png'));
+});
+
+// Основна команда
+gulp.task('default', gulp.parallel('pug', 'sass', 'images'));
---
+++ labs/lab10/lab9(4)/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+
+// Команда для компіляції Pug
+gulp.task('pug', function() {
+    return gulp.src('src/pug/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('dist'));
+});
+
+// Команда для компіляції SCSS
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
+});
+
+// Основна команда
+gulp.task('default', gulp.parallel('pug', 'sass'));
