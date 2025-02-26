be139a842c6f6a36e3e02b87aea971858ac5a8e4
dhorinyuk <dima.horinyuk27@gmail.com>
2025-02-26 12:54:17 +0200

    Download lab1sem2

---
+++ labs/lab1sem2/lab1/script.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName(("message")[0].innerText = "Hello world!";
+
+document.getElementsByClassName("button")[0].addEventListener("click", function() {
+    alert("Кнопка натиснута!");
+});
+
+function showStudentName() {
+    console.error("Ім'я студента: Діма");
+}
+
+document.getElementsByClassName("myDiv")[0].addEventListener("mouseover", showStudentName);
